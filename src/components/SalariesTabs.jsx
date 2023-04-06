1a2b6a1649738aca36a75119b7f05fbb118bce89
Mateo Cerquetella <mateocerquetella@hotmail.com>
2023-04-06 19:22:45 -0300

    Add a custom hook to track the active section and update the active tab accordingly in SalariesTabs component

--- src/components/SalariesTabs.jsx
+++ src/components/SalariesTabs.jsx
@@ -1,15 +1,62 @@
 import { TabList, Tab } from '@tremor/react'
 import { IconCash, IconUsersGroup, IconFilter } from '@tabler/icons-react'
+import { useEffect, useState } from 'react'
+
+export function useActiveSection (sectionIds, options) {
+  const [activeSection, setActiveSection] = useState(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const visibleSection = entries.find((entry) => entry.isIntersecting)
+
+        if (visibleSection) {
+          setActiveSection(visibleSection.target.id)
+        }
+      },
+      { ...options, threshold: 0, rootMargin: '0px 0px -100% 0px' } // Update rootMargin
+    )
+
+    sectionIds.forEach((id) => {
+      const section = document.getElementById(id)
+      if (section) {
+        observer.observe(section)
+      }
+    })
+
+    return () => {
+      sectionIds.forEach((id) => {
+        const section = document.getElementById(id)
+        if (section) {
+          observer.unobserve(section)
+        }
+      })
+    }
+  }, [sectionIds, options])
+
+  return activeSection
+}
 
 export function SalariesTabs () {
+  const [activeTab, setActiveTab] = useState('salaries-general')
+  const sectionIds = ['salaries-general', 'salaries-users', 'salaries-filter']
+  const activeSection = useActiveSection(sectionIds)
+
+  useEffect(() => {
+    if (activeSection) {
+      setActiveTab(activeSection)
+    }
+  }, [activeSection])
+
   const handleNavigation = (hash) => {
+    setActiveTab(hash)
     location.hash = hash
-    document.getElementById(`#${hash}`)?.scrollIntoView({ behavior: 'smooth' })
+    document.getElementById(hash)?.scrollIntoView({ behavior: 'smooth' })
   }
 
   return (
     <TabList
-      defaultValue='salaries-general'
+      value={activeTab}
       onValueChange={(value) => handleNavigation(value)}
       className='sticky top-0 z-50 justify-center mt-6 text-center bg-white'
     >
