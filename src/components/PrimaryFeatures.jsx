8af47b0e015781b5b5aad5fe1684978e3a2672b8
Ivan Telleria <ivantelleria96@gmail.com>
2023-04-08 22:19:13 +0200

    Created dataAndFeatures.js file and save in constants from Happiness and PrimaryFeatures components

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -1,59 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useResponsive } from '@/hooks/useResponsive'
 import Image from 'next/image'
 import { Tab } from '@headlessui/react'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
 import backgroundImage from '@/images/background-features.jpg'
-import screenshotExpenses from '@/images/screenshots/expenses.png'
-import screenshotPayroll from '@/images/screenshots/payroll.png'
-import screenshotReporting from '@/images/screenshots/reporting.png'
-import screenshotVatReturns from '@/images/screenshots/vat-returns.png'
-
-const features = [
-  {
-    title: 'Payroll',
-    description:
-      "Keep track of everyone's salaries and whether or not they've been paid. Direct deposit not supported.",
-    image: screenshotPayroll
-  },
-  {
-    title: 'Claim expenses',
-    description:
-      "All of your receipts organized into one place, as long as you don't mind typing in the data by hand.",
-    image: screenshotExpenses
-  },
-  {
-    title: 'VAT handling',
-    description:
-      "We only sell our software to companies who don't deal with VAT at all, so technically we do all the VAT stuff they need.",
-    image: screenshotVatReturns
-  },
-  {
-    title: 'Reporting',
-    description:
-      'Easily export your data into an Excel spreadsheet where you can do whatever the hell you want with it.',
-    image: screenshotReporting
-  }
-]
+import { features } from '@/constants/dataAndFeatures'
 
 export function PrimaryFeatures () {
-  const [tabOrientation, setTabOrientation] = useState('horizontal')
-
-  useEffect(() => {
-    const lgMediaQuery = window.matchMedia('(min-width: 1024px)')
-
-    function onMediaQueryChange ({ matches }) {
-      setTabOrientation(matches ? 'vertical' : 'horizontal')
-    }
-
-    onMediaQueryChange(lgMediaQuery)
-    lgMediaQuery.addEventListener('change', onMediaQueryChange)
-
-    return () => {
-      lgMediaQuery.removeEventListener('change', onMediaQueryChange)
-    }
-  }, [])
+  const { tabOrientation } = useResponsive()
 
   return (
     <section
