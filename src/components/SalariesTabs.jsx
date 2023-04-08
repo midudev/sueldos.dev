182b936adc196c36c17b4bd5f55f164b060dc598
Martín <maguera37@gmail.com>
2023-04-08 18:19:49 -0300

    feat: added use-active-section and use-active-tab hooks

--- src/components/SalariesTabs.jsx
+++ src/components/SalariesTabs.jsx
@@ -1,58 +1,12 @@
 import { TabList, Tab } from '@tremor/react'
 import { IconCash, IconUsersGroup, IconFilter } from '@tabler/icons-react'
-import { useEffect, useState } from 'react'
-
-export function useActiveSection (sectionIds, options) {
-  const [activeSection, setActiveSection] = useState(null)
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const visibleSection = entries.find((entry) => entry.isIntersecting)
-
-        if (visibleSection) {
-          setActiveSection(visibleSection.target.id)
-        }
-      },
-      { ...options, threshold: 0, rootMargin: '0px 0px -100% 0px' }
-    )
-
-    sectionIds.forEach((id) => {
-      const section = document.getElementById(id)
-      if (section) {
-        observer.observe(section)
-      }
-    })
-
-    return () => {
-      sectionIds.forEach((id) => {
-        const section = document.getElementById(id)
-        if (section) {
-          observer.unobserve(section)
-        }
-      })
-    }
-  }, [sectionIds, options])
-
-  return activeSection
-}
+import { useActiveSection } from '@/hooks/useActiveSection'
+import { useActiveTab } from '@/hooks/useActiveTab'
 
 export function SalariesTabs () {
-  const [activeTab, setActiveTab] = useState('salaries-general')
   const sectionIds = ['salaries-general', 'salaries-users', 'salaries-filter']
-  const activeSection = useActiveSection(sectionIds)
-
-  useEffect(() => {
-    if (activeSection) {
-      setActiveTab(activeSection)
-    }
-  }, [activeSection])
-
-  const handleNavigation = (hash) => {
-    setActiveTab(hash)
-    location.hash = hash
-    document.getElementById(hash)?.scrollIntoView({ behavior: 'smooth' })
-  }
+  const { activeSection } = useActiveSection(sectionIds)
+  const { activeTab, handleNavigation } = useActiveTab(activeSection)
 
   return (
     <TabList
