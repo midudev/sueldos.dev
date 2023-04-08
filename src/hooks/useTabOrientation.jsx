6679ca5700373bda0658d66f27beaee5f55ba1d5
Martín <maguera37@gmail.com>
2023-04-08 17:43:24 -0300

    feat: created use-tab-orientation hook

---
+++ src/hooks/useTabOrientation.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+export function useTabOrientation() {
+  const [tabOrientation, setTabOrientation] = useState('horizontal')
+
+  useEffect(() => {
+    const lgMediaQuery = window.matchMedia('(min-width: 1024px)')
+
+    function onMediaQueryChange ({ matches }) {
+      setTabOrientation(matches ? 'vertical' : 'horizontal')
+    }
+
+    onMediaQueryChange(lgMediaQuery)
+
+    lgMediaQuery.addEventListener('change', onMediaQueryChange)
+
+    return () => lgMediaQuery.removeEventListener('change', onMediaQueryChange)
+  }, [])
+
+  return { tabOrientation }
+}
