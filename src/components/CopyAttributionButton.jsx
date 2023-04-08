3169fcd7c2ff44a6b4fa9df1d652179514b6c036
Martín <maguera37@gmail.com>
2023-04-08 18:14:26 -0300

    feat: added use-copied-text hook

--- src/components/CopyAttributionButton.jsx
+++ src/components/CopyAttributionButton.jsx
@@ -1,23 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useCopiedText } from '@/hooks/useCopiedText'
+
 import { IconCopy, IconCheck } from '@tabler/icons-react'
 import { Button } from '@/components/Button'
 
-const ATTRIBUTION_TEXT = 'Datos extraídos de <a href="https://sueldos.dev/">sueldos.dev</a>'
-
 export function CopyAttributionButton () {
-  const [copied, setCopied] = useState(false)
-
-  useEffect(() => {
-    if (!copied) return
-
-    const timeoutId = setTimeout(() => setCopied(false), 2500)
-
-    return () => clearTimeout(timeoutId)
-  }, [copied])
-
-  const handleCopyAttribution = () => {
-    navigator.clipboard.writeText(ATTRIBUTION_TEXT).then(() => setCopied(true))
-  }
+  const {copied, handleCopyAttribution}  = useCopiedText()
 
   return (
     <Button onClick={handleCopyAttribution} className='flex gap-2 items-center justify-center my-6 md:mx-auto self-center text-sm'>
