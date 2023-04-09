34e734dac368996c8178554e3f7f03eb800f3e0f
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2023-04-09 19:37:48 +0200

    refactor: improve animated numbers component

--- src/components/AnimatedNumbers/index.jsx
+++ src/components/AnimatedNumbers/index.jsx
@@ -1,66 +1,35 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import NumbersList from './NumbersList'
 import { useNumberFormat } from '@/hooks/useFormatNumber'
 
-import classes from './animated-numbers.module.css'
-
-export default function AnimatedNumbers ({ number, locale = 'es-ES', currency, maximumFractionDigits = 0 }) {
+export default function AnimatedNumbers ({ number, dynamicFont, locale = 'es-ES', currency, maximumFractionDigits = 0 }) {
+  const numbersRef = useRef(null)
+  const [loaded, setLoaded] = useState(false)
   const { formatNumber } = useNumberFormat({ locale, currency, maximumFractionDigits })
 
-  const [animatedNumber, setAnimatedNumber] = useState(formatNumber(0))
+  const numbers = formatNumber(number).split('')
 
   useEffect(() => {
-    const timeoutIds = []
-    const maxDelay = 2000
-    let delay = 250
-    const step = (number * delay) / maxDelay
-
-    for (let value = 0; value <= number; value += step) {
-      const timeoutId = setTimeout(() => {
-        setAnimatedNumber(formatNumber(value))
-      }, delay)
-      timeoutIds.push(timeoutId)
-      delay++
-    }
-    return () => {
-      timeoutIds.forEach(timeoutId => clearTimeout(timeoutId))
-    }
-  }, [number])
-
-  return (
-    <span className='flex flex-row relative overflow-hidden'>
-      {
-        animatedNumber
-          .split('')
-          .map((value, index) => {
-            const isSpace = /\s/.test(value)
-            if (isNaN(+value) || isSpace) return value
-
-            return (
-              <AnimatedNumber
-                key={crypto.randomUUID()}
-                value={+value}
-                animationReverse={(+index % 2) === 0}
-              />
-            )
-          })
+    const observer = new IntersectionObserver((entries) => {
+      const isIntersecting = entries.find((entry) => entry.isIntersecting)
+      if (isIntersecting) {
+        setLoaded(true)
+        observer.unobserve(numbersRef.current)
       }
-    </span>
-  )
-}
+    })
 
-function AnimatedNumber ({ value, animationReverse }) {
-  const nextValue = value + 1 > 9 ? 0 : value + 1
-  const prevValue = value - 1 < 0 ? 9 : value - 1
+    observer.observe(numbersRef.current)
+
+    return () => {
+      observer.unobserve(numbersRef.current)
+    }
+  }, [])
+
   return (
-    <span
-      data-prev-number={prevValue}
-      data-next-number={nextValue}
-      className={`
-        ${classes.number}
-        ${animationReverse ? classes['number--reverse'] : ''}
-      `}
-    >
-      {value}
+    <span ref={numbersRef}>
+      {
+        loaded && <NumbersList numbers={numbers} dynamicFont={dynamicFont} />
+      }
     </span>
   )
 }
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -197,7 +197,7 @@ export function Filters () {
             <div className='flex flex-col items-center'>
               <strong className='text-7xl md:text-8xl lg:text-9xl'>
                 <span className='flex'>
-                  <AnimatedNumbers number={result.salary} currency='EUR' />
+                  <AnimatedNumbers number={result.salary} currency='EUR' dynamicFont />
                 </span>
               </strong>
               <small className='flex mt-4 text-sm text-gray-500 gap-x-1'>Basado en <AnimatedNumbers number={result.count} /> resultados</small>
