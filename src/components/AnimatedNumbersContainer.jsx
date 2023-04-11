fd6616fc7ca3fcb51a80f964ba1c5ca4e0e09938
kewinbarboza <desarrollo.merbpa@gmail.com>
2023-04-11 11:31:30 -0400

    add description button copy

--- src/components/AnimatedNumbersContainer.jsx
+++ src/components/AnimatedNumbersContainer.jsx
@@ -16,16 +16,18 @@ const AnimatedNumbersContainer = ({ result }) => {
       <strong className='text-7xl md:text-8xl lg:text-9xl'>
         <span className='flex flex-col md:flex-row'>
           <div className='flex'><AnimatedNumbers locale='es-ES' includeComma animateToNumber={result.salary} />€</div>
-          <Button
-            onClick={handleCopy}
-            variant='outline'
-            className=' self-center mt-3 md:mt-6 md:self-start'
-          >
-            {copied ? <IconCheck /> : <IconCopy />}
-          </Button>
         </span>
       </strong>
       <small className='flex mt-4 text-sm text-gray-500 gap-x-1'>Basado en <AnimatedNumbers locale='es-ES' includeComma animateToNumber={result.count} /> resultados</small>
+      <div className='flex justify-center'>
+        <Button
+          onClick={handleCopy}
+          variant='outline'
+          className='items-center gap-x-2 self-center mt-3 md:mt-6 md:self-start'
+        >
+          Copiar salario {copied ? <IconCheck /> : <IconCopy />}
+        </Button>
+      </div>
     </div>
   )
 }
